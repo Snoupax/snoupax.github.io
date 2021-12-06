f45d9e4fd4300e7510849ce4a80263096482d97a
Snoupax <snowpy1544@gmail.com>
2021-12-06 15:18:19 +0100

    portefolio tempo + js creation

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,3 +1,40 @@
+
+// A tab for Nav
+// One page 
+
+function openPage(pageName,elmnt,color) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tabContent");
+
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tabLink");
+
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].style.backgroundColor = "";
+    }
+
+
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+// Trigger Off for nav Mobile 
+function triggerOff(){
+    document.getElementById('navi').checked = false;
+    }
+
+
+
+
+// A tab for experience page
+// ExpPage
 function ExpPage(pageName,elmnt,color) {
     var i, tabExp, tabLinkExp;
     tabExp = document.getElementsByClassName("ExpContent");
@@ -8,7 +45,7 @@ function ExpPage(pageName,elmnt,color) {
 
     tabLinksExp = document.getElementsByClassName("tabLinkExp");
 
-    for (i = 0; i < tabLinkExp.length; i++) {
+    for (i = 0; i < tabLinksExp.length; i++) {
         tabLinksExp[i].style.backgroundColor = "";
     }
 
